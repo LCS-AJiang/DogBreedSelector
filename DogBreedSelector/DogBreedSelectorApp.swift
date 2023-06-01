91864557fb766b8bda22e0cb4c3691bb2193dd28
Audrey Jiang <ajiang@lcs.on.ca>
2023-06-01 14:19:02 -0400

    Initial Commit

---
+++ DogBreedSelector/DogBreedSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedSelectorApp.swift
+//  DogBreedSelector
+//
+//  Created by Audrey Jiang on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
